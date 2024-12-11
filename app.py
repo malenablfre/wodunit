a1dd66d1cf34979362efa923074fa79780ba7c31
Anna-Lotta <188203833+annalotta123@users.noreply.github.com>
2024-12-11 13:52:14 +0100

    test screen (works)

--- app.py
+++ app.py
@@ -2,8 +2,14 @@ from flet import *
 from views.routes import views_handler
 
 def main(page: Page):
+    page.title = "Home"
+    page.window_width = 400
+    page.window_height = 700
+    page.window_resizable = False
+    page.vertical_alignment = MainAxisAlignment.CENTER
+    page.horizontal_alignment = CrossAxisAlignment.CENTER
+    #page.bgcolor = "#2D142C"
     
-
     def route_change(route):
         print(page.route)
         page.views.clear()
--- views/home_view.py
+++ views/home_view.py
@@ -1,25 +1,59 @@
-from flet import *
+import flet as ft
 
-class home(UserControl):
+class home(ft.UserControl):
     def __init__(self, page):
         super().__init__()
         self.page = page
 
     def build(self):
-        return Column(
-            controls=[
-                Container(
-                    height=800,
-                    width=350,
-                    bgcolor="red",
-                    content=Column(
-                        controls=[
-                            Text('Welcome to the homepage'),
-                            Container(
-                                content=TextButton("Go to Login", on_click=lambda _: self.page.go("/login"))
-                            )
-                        ]
-                    )
-                )
-            ]
-        )
+        page = ft.Container(
+                ft.Column(
+                spacing=0,
+                controls=[
+                    ft.Text(value="Whodunit?", size= 50, font_family= "Times New Roman", weight= "bold", color="#EE4540"),
+                    ft.Text(value="Das Spiel des Mörders", size= 25, font_family= "Times New Roman", weight= "bold", color="#801336"),
+
+                    ft.Container(
+                        content=ft.Text(value="Aktive Spiele", size= 20, font_family= "Times New Roman", weight= "bold", color="#C72C42"),
+                        margin=10,
+                        padding=10,
+                        alignment=ft.alignment.center,
+                        bgcolor="#510A32",
+                        width=250,
+                        height=50,
+                        border_radius=10,
+                        on_click=lambda _: self.page.go("/login")
+                    ),
+
+                    ft.Container(
+                        content=ft.Text(value="Spiel beitreten", size= 20, font_family= "Times New Roman", weight= "bold", color="#C72C42"),
+                        margin=10,
+                        padding=10,
+                        alignment=ft.alignment.center,
+                        bgcolor="#510A32",
+                        width=250,
+                        height=50,
+                        border_radius=10,
+                        #on_click=
+                    ),
+
+                    ft.Container(
+                        content=ft.Text(value="Übersicht", size= 20, font_family= "Times New Roman", weight= "bold", color="#C72C42"),
+                        margin=10,
+                        padding=10,
+                        alignment=ft.alignment.center,
+                        bgcolor="#510A32",
+                        width=250,
+                        height=50,
+                        border_radius=10,
+                        #on_click=
+                    ),
+                ],
+                alignment=ft.MainAxisAlignment.CENTER,
+            ),
+            width=400,
+            height=700,
+            bgcolor="#2D142C",
+            border_radius=10
+            )
+        return page
