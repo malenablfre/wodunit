aea6224eb4eadf623e79ead7f09bf7d3514ee595
Anna-Lena <188204422+anna-lena18@users.noreply.github.com>
2024-12-12 14:16:50 +0100

    Text

--- pages/Spielregeln.py
+++ pages/Spielregeln.py
@@ -1,4 +1,6 @@
-class home(ft.UserControl):
+import flet as ft 
+
+class Spielregeln(ft.UserControl):
     def __init__(self, page):
         super().__init__()
         self.page = page
@@ -24,44 +26,25 @@ class home(ft.UserControl):
                         content=ft.Text(value="Aktive Spiele", size= 20, font_family= "Times New Roman", weight= "bold", color="#C72C42"),
                         margin=10,
                         padding=10,
-                        alignment=ft.alignment.center,
-                        bgcolor="#510A32",
-                        width=250,
-                        height=50,
-                        border_radius=10,
-                        on_click=lambda _: self.page.go("/login")
                     ),
 
                     ft.Container(
                         content=ft.Text(value="Spiel beitreten", size= 20, font_family= "Times New Roman", weight= "bold", color="#C72C42"),
                         margin=10,
                         padding=10,
-                        alignment=ft.alignment.center,
-                        bgcolor="#510A32",
-                        width=250,
-                        height=50,
-                        border_radius=10,
-                        #on_click=
                     ),
 
                     ft.Container(
                         content=ft.Text(value="Übersicht", size= 20, font_family= "Times New Roman", weight= "bold", color="#C72C42"),
                         margin=10,
                         padding=10,
-                        alignment=ft.alignment.center,
-                        bgcolor="#510A32",
-                        width=250,
-                        height=50,
-                        border_radius=10,
-                        on_click=lambda _: self.page.go("/Uebersicht")
                     ),
                 ],
                 alignment = ft.MainAxisAlignment.CENTER,
+                horizontal_alignment=ft.CrossAxisAlignment.CENTER,
             ),
             width=400,
             height=700,
-            #bgcolor="#2D142C",
-            #gradient=ft.RadialGradient(["#2D142C","#510A32"]),
             gradient=ft.LinearGradient(
                 begin=ft.alignment.top_center,
                 end=ft.alignment.bottom_center,
