c2a0a7260c05f0f7f3d15b401c3236e16d88274e
Anna-Lena <188204422+anna-lena18@users.noreply.github.com>
2024-12-12 14:15:25 +0100

    navigation

--- pages/routes.py
+++ pages/routes.py
@@ -2,6 +2,8 @@ import flet as ft
 from pages.home import home
 from pages.login import login
 from pages.Uebersicht import Uebersicht 
+from pages.Rollenuebersicht import Rollenuebersicht
+from pages.Spielregeln import Spielregeln
 
 def views_handler(page):
     return {
@@ -21,6 +23,18 @@ def views_handler(page):
             route='/Uebersicht',
             controls=[
                 Uebersicht(page)
+            ]
+        ), 
+        '/Rollenuebersicht':ft.View(
+            route='/Rollenuebersicht',
+            controls=[
+                Rollenuebersicht(page)
+            ]
+        ), 
+        '/Spielregln':ft.View(
+            route='/Spielregln',
+            controls=[
+                Spielregeln(page)
             ]
         )
     }
