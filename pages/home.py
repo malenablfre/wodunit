4c0f1dedb5bba9dcb49724b092912a6b961cbcab
Anna-Lena <188204422+anna-lena18@users.noreply.github.com>
2024-12-12 12:53:24 +0100

    navgation

--- pages/home.py
+++ pages/home.py
@@ -55,7 +55,7 @@ class home(ft.UserControl):
                         width=250,
                         height=50,
                         border_radius=10,
-                        #on_click=
+                        on_click=lambda _: self.page.go("/Uebersicht")
                     ),
                 ],
                 alignment = ft.MainAxisAlignment.CENTER,
