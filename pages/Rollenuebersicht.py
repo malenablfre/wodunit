5985a565c7448e1490161f2300eaf3c91424f9fb
Anna-Lena <188204422+anna-lena18@users.noreply.github.com>
2024-12-18 11:05:34 +0100

    zurück Botton

--- pages/Rollenuebersicht.py
+++ pages/Rollenuebersicht.py
@@ -6,11 +6,23 @@ class Rollenuebersicht(ft.UserControl):
         self.page = page
 
     def build(self):
-        page = ft.Container(
-                ft.Column(
-                spacing=0,
-                scroll=ft.ScrollMode.ALWAYS,
-                controls=[
+        back_container = ft.Container(
+            content=ft.Text(value="↩", size=20, font_family="Times New Roman", weight="bold", color="#C72C42"),
+            margin=10,
+            padding=10,
+            alignment=ft.alignment.center,
+            bgcolor="#510A32",
+            width=50,
+            height=50,
+            border_radius=10,
+            ink=True,
+            on_click=lambda _: self.page.go("/Uebersicht")
+        )
+
+        other_containers = ft.Column(
+              spacing=10,
+              scroll=ft.ScrollMode.ALWAYS,
+              controls=[
                     ft.Container(
                         content=ft.Text(value="Rollenübersicht", size= 50, font_family= "Times New Roman", weight= "bold", color="#EE4540"),
                         margin=5,
@@ -46,14 +58,26 @@ class Rollenuebersicht(ft.UserControl):
                 ],
                 alignment = ft.MainAxisAlignment.CENTER,
                 horizontal_alignment=ft.CrossAxisAlignment.CENTER,
-            ),
+            )
+        
+        background_container = ft.Container(
+            content=other_containers,
             width=400,
             height=700,
             gradient=ft.LinearGradient(
                 begin=ft.alignment.top_center,
                 end=ft.alignment.bottom_center,
-                colors=["#2D142C","#510A32"]),
+                colors=["#2D142C","#510A32"]
+            ),
             border_radius=10,
             alignment=ft.alignment.center
-            )
+        )
+
+        page = ft.Stack(
+            controls=[
+                background_container,  
+                back_container,
+            ]
+        )
+
         return page
--- pages/Spielregeln.py
+++ pages/Spielregeln.py
@@ -1,4 +1,4 @@
-import flet as ft 
+import flet as ft
 
 class Spielregeln(ft.UserControl):
     def __init__(self, page):
@@ -6,14 +6,26 @@ class Spielregeln(ft.UserControl):
         self.page = page
 
     def build(self):
-        page = ft.Container(
-                ft.Column(
-                spacing=0,
-                scroll=ft.ScrollMode.ALWAYS,
-                controls=[
+        back_container = ft.Container(
+            content=ft.Text(value="↩", size=20, font_family="Times New Roman", weight="bold", color="#C72C42"),
+            margin=10,
+            padding=10,
+            alignment=ft.alignment.center,
+            bgcolor="#510A32",
+            width= 50,
+            height=50,
+            border_radius=10,
+            ink=True,
+            on_click=lambda _: self.page.go("/Uebersicht")
+        )
+
+        other_containers = ft.Column(
+              spacing=10,
+              scroll=ft.ScrollMode.HIDDEN,
+              controls=[
                     ft.Container(
-                        content=ft.Text(value="Die 13 Gebote", size= 50, font_family= "Times New Roman", weight= "bold", color="#EE4540"),
-                        margin=10,
+                        content=ft.Text(value="Die 13 Gebote", size= 40, font_family= "Times New Roman", weight= "bold", color="#EE4540"),
+                        margin= 20,
                         padding=10,
                     ),
 
@@ -90,6 +102,7 @@ class Spielregeln(ft.UserControl):
                         content=ft.Text(value="7.   Tötungsstopp bei Meeting:", size= 25, font_family= "Times New Roman", weight= "bold", color="#C72C42"),
                         margin=10,
                         padding=10,
+                        alignment=ft.alignment.center_right
                     ),
 
                     ft.Container(
@@ -100,7 +113,6 @@ class Spielregeln(ft.UserControl):
                     ft.Container(
                         content=ft.Text(value="8.   Missbrauch des Emergency Buttons:", size= 25, font_family= "Times New Roman", weight= "bold", color="#C72C42"),
                         margin=10,
-                        padding=10,
                     ),
 
                     ft.Container(
@@ -163,18 +175,42 @@ class Spielregeln(ft.UserControl):
                         margin=10,
                         padding=10,
                     ),
-
                 ],
                 alignment = ft.MainAxisAlignment.CENTER,
-            
-            ),
+                horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+            )
+        # ft.Column(
+        #     controls=[
+        #         ft.Container(),
+        #         ft.Row(
+        #             controls=[
+        #                 ft.Container(content=ft.IconButton(ft.Icons.ARROW_BACK, icon_color="#EE4540", on_click=lambda _: self.page.go("/Uebersicht")),),
+        #                 ft.Container(content=ft.Text(value="Die 13 Gebote", size= 35, font_family= "Times New Roman", weight= "bold", color="#EE4540"),),
+        #                 ft.Container(content=ft.IconButton(ft.Icons.MENU, icon_color="#EE4540", on_click=lambda _: self.page.go("/Uebersicht")),),
+        #             ],
+        #             alignment=ft.MainAxisAlignment.SPACE_AROUND,
+        #         )
+        #     ]
+        # )
+        
+        background_container = ft.Container(
+            content=other_containers,
             width=400,
             height=700,
             gradient=ft.LinearGradient(
                 begin=ft.alignment.top_center,
                 end=ft.alignment.bottom_center,
-                colors=["#2D142C","#510A32"]),
+                colors=["#2D142C","#510A32"]
+            ),
             border_radius=10,
-            alignment=ft.alignment.center,
-            )
+            alignment=ft.alignment.center
+        )
+        
+        page = ft.Stack(
+            controls=[
+                background_container,  
+                back_container,
+            ]
+        )
+
         return page
